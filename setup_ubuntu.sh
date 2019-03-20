805fc24d2536794b586c178a0cb646f8781fde1e
Helal Khan <helal86@users.noreply.github.com>
2019-03-20 21:19:40 +0000

    fixes

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -8,12 +8,10 @@ echo $username
 
 sudo apt update
 sudo apt upgrade -y 
-sudo apt install -y curl wget lsb-release dirmngr snapd
+sudo apt install -y curl wget lsb-release dirmngr snapd apt-transport-https lsb-release software-properties-common
 
 #add repositories 
 sudo add-apt-repository -y ppa:numix/ppa
-sudo add-apt-repository -y ppa:tualatrix/ppa
-sudo add-apt-repository -y ppa:nvbn-rm/ppa
 sudo add-apt-repository -y ppa:shutter/ppa
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo add-apt-repository -y ppa:peterlevi/ppa
@@ -31,13 +29,20 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
 
 #VS Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+#Virtual Box
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list.d/virtualbox.list'
 
 sudo apt-get update
 
@@ -45,8 +50,7 @@ sudo apt-get update
 sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle 
 
 #Install standard packages
-sudo apt-get install -y software-properties-common apt-transport-https curl ca-certificates android-tools-adb android-tools-fastboot docker-ce build-essential filezilla filezilla-common g++ htop git git-core git-man gparted gvncviewer java-common postgresql-client postgresql-client-common python2.7-dev rdesktop screen gnome-tweak-tool gnome-tweaks unzip unrar vlc whois openssh-server openjdk-8-jdk p7zip testdisk virtualbox python-
-exfat-utils ansible variety gimp shutter traceroute network-manager-openvpn network-manager-openvpn-gnome green-recorder azure-cli sshuttle jq code wine-stable winetricks chrome-gnome-shell xournal clipit
+sudo apt-get install -y ca-certificates android-tools-adb android-tools-fastboot docker-ce build-essential filezilla filezilla-common g++ htop git git-core git-man gparted gvncviewer java-common postgresql-client postgresql-client-common python2.7-dev rdesktop screen gnome-tweak-tool gnome-tweaks unzip unrar vlc whois openssh-server openjdk-8-jdk p7zip testdisk virtualbox exfat-utils ansible variety gimp shutter traceroute network-manager-openvpn network-manager-openvpn-gnome green-recorder azure-cli sshuttle jq code wine-stable winetricks chrome-gnome-shell xournal clipit python3 chromium-browser net-tools 
 
 #snap install 
 sudo snap install skype --classic
@@ -58,10 +62,6 @@ pip install virtualenv
 #Installing Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-#Installing google-talk-plugin
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google-talkplugin.list'
 
 #Dropbox
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
@@ -89,8 +89,8 @@ sudo dpkg -i stacer_1.0.9_amd64.deb
 
 #Install Terraform
 cd ~/Downloads
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+unzip terraform_0.11.11_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
 #Install Vagrant
@@ -149,6 +149,3 @@ sudo sed -i "s/Icon=code/Icon=vscode/g" /usr/share/applications/code.desktop
 
 #disable online search via gnome tweak
 gnome-tweaks
-
-
-
